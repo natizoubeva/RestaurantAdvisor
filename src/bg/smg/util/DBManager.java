1e5759aa6be61673999b6613a8868b16ba24a78f
natizoubeva <natalia.zoubeva@gmail.com>
2023-12-31 16:53:55 +0200

    Initial commit

---
+++ src/bg/smg/util/DBManager.java
@@ -0,0 +1,37 @@
+package bg.smg.util;
+
+/**
+ *
+ * @author n.m.borisova
+ */
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class DBManager {
+
+    private static DBManager instance;
+    private final DataSource dataSource;
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    DBManager() throws SQLException {
+        MariaDbDataSource dataSource = new MariaDbDataSource();
+        dataSource.setUrl("jdbc:mariadb://localhost:3306/bank_ms");
+        dataSource.setUser("root");
+        dataSource.setPassword(null);
+        this.dataSource = dataSource;
+    }
+
+    public static synchronized DBManager getInstance() throws SQLException {
+        if(instance == null) {
+            instance = new DBManager();
+        }
+        return instance;
+    }
+
+}
